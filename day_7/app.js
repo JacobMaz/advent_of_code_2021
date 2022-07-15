0bf65a2ae0e4711f6da9f4daad37bdb41f8d308f
JakeMaz <jakemazanowski@gmail.com>
2022-07-15 11:17:43 -0400

    day_7 solved

--- day_7/app.js
+++ day_7/app.js
@@ -6,6 +6,28 @@ const data = fs.readFileSync('data.txt', {encoding: 'utf-8'})
     .filter((x)=>Boolean(x))
     .map((x)=>(+x));
 
+// ** PART ONE
+
+// let crabs_at_each_x_point = Array(Math.max(...data)+1).fill(0)
+
+// for(let i=0; i<data.length; i++){
+//     crabs_at_each_x_point[data[i]]++
+// }
+
+// let total_fuel_used_to_point = Array(Math.max(...data)).fill(0);
+
+// for(let point=0; point<crabs_at_each_x_point.length; point++){
+//     for(let fuel_point=0; fuel_point<total_fuel_used_to_point.length; fuel_point++){
+//         total_fuel_used_to_point[fuel_point] += (Math.max(point, fuel_point) - Math.min(point, fuel_point)) * crabs_at_each_x_point[point]
+//     }
+// }
+
+// console.log(Math.min(...total_fuel_used_to_point))
+
+// ** PART TWO
+
+const numSum = (n) => n * (n+1) / 2;
+
 let crabs_at_each_x_point = Array(Math.max(...data)+1).fill(0)
 
 for(let i=0; i<data.length; i++){
@@ -15,10 +37,10 @@ for(let i=0; i<data.length; i++){
 let total_fuel_used_to_point = Array(Math.max(...data)).fill(0);
 
 for(let point=0; point<crabs_at_each_x_point.length; point++){
-    for(let crab=1; crab<=crabs_at_each_x_point[point]; crab++){
-        for(let fuel_point=0; fuel_point<total_fuel_used_to_point.length; fuel_point++){
-            total_fuel_used_to_point[fuel_point] += (Math.max(point, fuel_point) - Math.min(point, fuel_point))
-        }
+    for(let fuel_point=0; fuel_point<total_fuel_used_to_point.length; fuel_point++){
+        let distance_to_point = (Math.max(point, fuel_point) - Math.min(point, fuel_point))
+        let fuel_to_point = numSum(distance_to_point) * crabs_at_each_x_point[point]
+        total_fuel_used_to_point[fuel_point] += fuel_to_point
     }
 }
 
